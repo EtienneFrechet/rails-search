7b51121ffea02e2e9dfdfb3468fee5582f8b6d8d
etienne frechet <efrechet@hotmail.fr>
2024-01-17 14:04:59 +0800

    home page is first draft with different colors, newtas page is second with proper tas colors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get '/newtas', to: "pages#newtas"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
